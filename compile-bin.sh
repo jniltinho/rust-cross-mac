2ee7eae9b31330cc3f0a8ee73416128da77ce37f
Nilton Oliveira <jniltinho@gmail.com>
2022-05-02 13:52:50 -0300

    Update compile-bin.sh

--- compile-bin.sh
+++ compile-bin.sh
@@ -7,20 +7,22 @@ FOLDER_BIN=$(pwd)/files/bin
 
 mkdir -p $FOLDER_BIN
 
-export MACOSX_CROSS_COMPILER=/opt
+export MACOSX_CROSS_COMPILER=/tmp
 export PATH=$PATH:/opt/osxcross/target/bin:$FOLDER_BIN
 
 # Install build tools
 apt-get update -qq
 apt-get install -yqq llvm-dev libclang-dev clang libxml2-dev git curl libz-dev zlib1g-dev libssl-dev make cmake unzip xz-utils
 
-cd /opt
+cd $MACOSX_CROSS_COMPILER
 git clone https://github.com/tpoechtrager/osxcross
 cd osxcross
 curl -skL https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.13.sdk.tar.xz -o tarballs/MacOSX10.13.sdk.tar.xz
 UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
-rm -f /opt/osxcross/tarballs/MacOSX*.sdk.tar.xz
+mkdir -p /opt/osxcross
+mv $MACOSX_CROSS_COMPILER/osxcross/build /opt/osxcross/; mv $MACOSX_CROSS_COMPILER/osxcross/target /opt/osxcross/
 
+cd /root
 curl -skLO https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
 tar -xf upx-3.9*-amd64_linux.tar.xz; cp upx-3.9*-amd64_linux/upx $FOLDER_BIN/
 chmod +x $FOLDER_BIN/upx
